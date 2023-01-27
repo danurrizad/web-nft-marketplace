8b6c98375049afdc4522b913c33bc2e0682755a7
danurrizad <danurrizad242@gmail.com>
2023-01-27 16:50:45 +0700

    update import

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import { useState } from 'react';
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false)
   return (
     <div className="App">
       <Header/>
-      <Content/>
+      <Content isConnected={isConnected} setIsConnected={setIsConnected}/>
       <Footer/>
     </div>
   );
