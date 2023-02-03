605348cbdac03de20b48ce83965558426e1b5439
danurrizad <danurrizad242@gmail.com>
2023-02-04 00:10:33 +0700

    add alchemy API for deploying network goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY = "STXVjIuY-qHdz6LMdoGlHu4RObMz6oxS"
+const GOERLI_PRIVATE_KEY = "8af5226655090452e88e8bc3ce8195c47b8823cd9b01f26e741df19d0c3a451f"
+
 module.exports = {
   solidity: "0.8.17",
   paths: {
@@ -9,4 +12,10 @@ module.exports = {
     cache: "./src/backend/cache",
     tests: "./src/backend/test"
   },
+  networks:{
+    goerli:{
+      url:`https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
