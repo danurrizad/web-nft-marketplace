2987eed0970a99fe00d78acc530e082a233575db
danurrizad <danurrizad242@gmail.com>
2023-02-08 23:41:53 +0700

    update for responsive items view

--- src/frontend/components/MyPurchases.jsx
+++ src/frontend/components/MyPurchases.jsx
@@ -1,11 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import { ethers } from 'ethers'
 
+import Loading from './Loading';
+
 
 const MyPurchases = ({account, isLoading, nft, marketplace}) => {
-    const [purchases, setPurchases] = useState([])
+    const [purchases, setPurchases] = useState([]);
+    const [isLoadingItems, setIsLoadingItems] = useState(false);
 
     const loadPurchasedItems = async () => {
+        setIsLoadingItems(true)
         const filter =  marketplace.filters.Bought(null,null,null,null,null,account)            // Fetch purchased items from marketplace by quering Offered events with the buyer set as the user
         const results = await marketplace.queryFilter(filter)
         const purchases = await Promise.all(results.map(async i => {        //Fetch metadata of each nft and add that to listedItem object.
@@ -25,6 +29,7 @@ const MyPurchases = ({account, isLoading, nft, marketplace}) => {
         return purchasedItem
         }))
         setPurchases(purchases)
+        setIsLoadingItems(false)
     }
 
     useEffect(() => {
@@ -46,32 +51,35 @@ const MyPurchases = ({account, isLoading, nft, marketplace}) => {
     <div>
         <div className='bg-slate-300 min-h-screen'>
             <div className='py-8 px-10'>
-                <h1 className='2xl:text-[70px] xl:text-[70px] lg:text-[70px] text-[50px] text-center'>My Purchases</h1>
+                <h1 className='2xl:text-[70px] xl:text-[70px] lg:text-[70px] text-[45px] text-center'>My Purchases</h1>
             </div>
-            <div>
-            <div className='justify-center flex-1 gap-y-10 2xl:flex 2xl:justify-center 2xl:gap-0 2xl:p-8 px-2'>
-                {purchases.length > 0 ? (
-                <div className="px-5 container">
-                    <div className="g-4 py-5">
-                        {purchases.map((item, index) => (
-                        <div key={index} className="overflow-hidden">
-                            <div className='pb-8 bg-slate-100 border-solid border-2 border-black w-[150px] 2xl:w-[300px] xl:w-[300px] 2xl:h-[420px] xl:h-[400px] h-[220px]'>
-                                <img variant="top" src={item.image} className=''/>
-                                <div className='text-center 2xl:text-[20px] text-[10px]'>
-                                    <div>Purchased for :{ethers.utils.formatEther(item.totalPrice)} ETH</div>
+            {isLoadingItems ? (<Loading textLoading='Loading items...'/>) : (
+                <div>
+                <div className='justify-center flex-1 gap-y-10 2xl:flex 2xl:justify-center 2xl:gap-0 2xl:p-8 px-2'>
+                    {purchases.length > 0 ? (
+                    <div className="px-5 container">
+                        <div className="g-4 py-5">
+                            {purchases.map((item, index) => (
+                            <div key={index} className="overflow-hidden">
+                                <div className='shadow-xl shadow-slate-600 bg-slate-700 border-solid border-2 border-b-0 border-black w-[150px] 2xl:w-[300px] xl:w-[300px] lg:w-[300px] md:w-[300px] sm:w-[250px] min-w-[150px]'>
+                                    <img variant="top" src={item.image} alt={"item-"+index} className=''/>
+                                    <div className='text-center text-[12px] 2xl:text-[20px] xl:text-[20px] lg:text-[20px] md:text-[20px] sm:text-[12px] p-4 text-white'>
+                                        <div>Purchased for {ethers.utils.formatEther(item.totalPrice)} ETH</div>
+                                    </div>
                                 </div>
                             </div>
+                            ))}
                         </div>
-                        ))}
+                    </div>)
+                    : (
+                    <div className=''>
+                        <h2 className='text-center 2xl:text-[30px] xl:text-[30px] lg:text-[30px] text-[20px]'>You don't have any purchases</h2>
                     </div>
-                </div>)
-                : (
-                <div className=''>
-                    <h2 className='text-center 2xl:text-[30px] xl:text-[30px] lg:text-[30px] text-[20px]'>You don't have any purchases</h2>
+                    )}
                 </div>
-                )}
-            </div>
-            </div>
+                </div>
+            )}
+            
         </div>
     </div>
   )
