23ea228a6271054e5aa4e0634f16562a8aa4f6e0
danurrizad <danurrizad242@gmail.com>
2023-02-08 23:38:28 +0700

    update loading

--- src/frontend/components/Home.jsx
+++ src/frontend/components/Home.jsx
@@ -4,12 +4,23 @@ import Navbar from './Navbar';
 import { ethers } from 'ethers';
 import ReactLoading from 'react-loading';
 import LandingPage from './Landing.jsx'
+import Loading from './Loading';
+import Modal from './Modal';
 
 
 const Home = ({account, nft, marketplace}) => {
     
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [showModal, setShowModal] = useState({
+        title:"",
+        img:"",
+        isRunning:false
+    });
+
+    const handleConfirm = (items) => {
+        setShowModal(true)
+    }
 
     const loadMarketplaceItems = async () => {
         setIsLoading(true);
@@ -47,30 +58,6 @@ const Home = ({account, nft, marketplace}) => {
       loadMarketplaceItems()
     }, [])
     
-    if(isLoading) return (
-        <>
-            <LandingPage/>
-            <div className='min-h-screen bg-slate-300'>
-                <section id='header-content'>
-                    <div>
-                        <h1 className='text-center text-[72px] pb-12'>Store</h1>
-                        <div className='justify-center flex-1 text-center px-4 2xl:text-[30px] xl:text-[30px] lg:text-[30px] md:text-[30px] text-[15px]'>
-                            <h1 className='pt-1'>Wallet address :</h1>
-                            <div className='pt-4 flex justify-center'>
-                                <div className='flex items-center'>
-                                    <ReactLoading type='spin' color='white' height={50} width={50}/>
-                                </div>
-                                <div className='flex items-center'>
-                                    <h1 className=' px-4 2xl:text-[50px] xl:text-[50px] lg:text-[50px] text-[20px] text-center'>Loading items...</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </>
-        
-      )
 
     return (
     <div>
@@ -78,12 +65,13 @@ const Home = ({account, nft, marketplace}) => {
         <LandingPage/>
         
         {/* STORE SECTION */}
-        <div id='store' className='min-h-screen bg-slate-300'>
+        <div id='store' className='min-h-screen bg-slate-300 max-w-screen'>
             <section id='header-content'>
                 <div>
                     <h1 className='text-center text-[72px] pb-12'>Store</h1>
                     <div className='justify-center flex-1 text-center px-4 2xl:text-[30px] xl:text-[30px] lg:text-[30px] md:text-[30px] text-[15px]'>
                         <h1 className='pt-1'>Wallet address :</h1>
+                        {isLoading ? <Loading textLoading='Loading items...'/> : null}
                         {account ? (<div className='flex justify-center'><h1 className='bg-slate-100 px-4 py-1 rounded-full shadow-lg shadow-black'>{account}</h1></div>) : null}
                         
                         <div className='px-4 2xl:text-[28px] xl:text-[28px] lg:text-[28px] md:text-[28px] text-[15px]'>
@@ -93,25 +81,27 @@ const Home = ({account, nft, marketplace}) => {
                 </div>
             </section>
             <section id='items-content'>
-                <div className='flex justify-center'>
+                <div className='flex justify-center '>
                     {items.length > 0 ? (
-                        <div className='flex justify-between'>
-                            <div className='justify-center flex gap-y-10 gap-x-10 2xl:flex 2xl:justify-center 2xl:p-8 py-10 px-2'>
+                        <div className='flex justify-center'>
+                            <div className='justify-center flex flex-wrap gap-y-10 gap-x-10 2xl:p-8 py-10 px-2'>
                                 {items.map((item,index) => (
-                                    <div key={index} className='pb-8 bg-slate-100 border-solid border-2 border-black w-[150px] 2xl:w-[300px] xl:w-[300px] 2xl:h-[420px] xl:h-[400px] h-[220px]'> 
+                                    <div key={index} className='shadow-xl shadow-slate-900 bg-slate-100 border-solid border-2 border-b-0 border-black w-[150px] 2xl:w-[300px] xl:w-[300px] h-fit'> 
+                                        <h1 className='text-center 2xl:text-[20px] text-[15px] bg-slate-700 text-white p-1'>{item.name}</h1>
                                         <img src={item.image} alt='NFT-1'/>
-                                        <h1 className='text-center 2xl:text-[20px] text-[15px]'>{item.name}</h1>
                                         <div className='text-center 2xl:text-[20px] text-[10px]'>
-                                            <p>{item.description}</p>
-                                            <div className=''>
-                                                <button className='bg-green-300 mt-2 2xl:py-1 xl:py-1 lg:py-1 md:py-1 sm:py-1 px-4 rounded-3xl hover:bg-green-500 duration-200'
-                                                    onClick={() => buyItems(item)}
+                                            <p className='text-left px-4'>{item.description}</p>
+                                            <div className='bg-slate-700 border-y-4 border-black p-4'>
+                                                <button className='bg-green-300 2xl:py-1 xl:py-1 lg:py-1 md:py-1 sm:py-1 px-4 rounded-3xl hover:bg-green-500 duration-200'
+                                                    onClick={()=>buyItems(item)}
                                                     >Buy ({ethers.utils.formatEther(item.totalPrice)} ETH)
                                                 </button>
                                             </div>
                                         </div>
                                     </div>
                                 ))}
+
+                                {/*{showModal ? <Modal onConfirm={confirmBuy}/> : null}*/}
                             </div>
                         </div>
                     ):(
