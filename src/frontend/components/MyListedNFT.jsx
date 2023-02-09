f2b83b71cd9e1783eb4d67e8ab78250fa1c73ec0
danurrizad <danurrizad242@gmail.com>
2023-02-09 20:44:42 +0700

    update msg and style

--- src/frontend/components/MyListedNFT.jsx
+++ src/frontend/components/MyListedNFT.jsx
@@ -51,11 +51,13 @@ const MyListedNFT = ({account, nft, marketplace}) => {
     function loadSoldItems(items){
       return(
         <>
-          <div className='flex'>
+          {items.length > 0 ? 
+          (<div className='flex'>
             <div className='px-10 py-8 flex flex-wrap'>
               {items.map((item, index) => (
                 <div key={index} className='bg-slate-100 border-solid border-2 border-black w-[150px] 2xl:w-[300px] xl:w-[300px] lg:w-[300px] min-w-[150px] h-fit '>
                   <div>
+                  <h1 className='text-center 2xl:text-[20px] text-[15px] bg-slate-700 text-white p-1'>{item.name}</h1>
                     <img src={item.image} alt={"sold-items-"} />
                     <div className=' shadow-xl shadow-slate-600 bg-slate-700 p-4 border-y-4 border-black flex flex-col text-center justify-center text-white 2xl:text-[23px] xl:text-[23px] lg:text-[23px] md:text-[20px] sm:text-[12px] text-[12px]'>
                       <p>Received : {ethers.utils.formatEther(item.price)} ETH</p>
@@ -64,7 +66,11 @@ const MyListedNFT = ({account, nft, marketplace}) => {
                 </div>
               ))}
             </div>
-          </div>
+          </div>)
+          :(
+          <div className='2xl:text-[30px] xl:text-[30px] lg:text-[30px] text-[20px]  pt-8 px-10 '>
+              <h1>You dont have any NFT that already sold</h1>
+          </div>)}
         </>
       )
     }
@@ -80,8 +86,9 @@ const MyListedNFT = ({account, nft, marketplace}) => {
                           <div className='px-10 py-8 flex flex-wrap gap-10'>
                               {listedItems.map((item,index) => (
                                   <div key={index} className='shadow-xl shadow-slate-600 bg-slate-700 border-solid border-2 border-b-0 border-black w-[150px] 2xl:w-[300px] xl:w-[300px] lg:w-[300px] min-w-[150px] h-fit '>
+                                      <h1 className='text-center 2xl:text-[20px] xl:text-[20px] lg:text-[20px] md:text-[15px] sm:text-[15px] text-[15px] bg-slate-700 text-white p-1'>{item.name}</h1>
                                       <div className='p-0'><img src={item.image}/></div>
-                                      <div className=' bg-slate-700 p-4 border-y-4 border-black flex justify-center text-white 2xl:text-[23px] xl:text-[23px] lg:text-[23px] md:text-[20px] sm:text-[12px] text-[12px]'>
+                                      <div className=' bg-slate-700 p-4 border-y-4 border-black flex justify-center text-white 2xl:text-[23px] xl:text-[23px] lg:text-[23px] md:text-[12px] sm:text-[12px] text-[12px]'>
                                         <h2>Listed for <span>{ethers.utils.formatEther(item.totalPrice)}</span> ETH</h2>
                                       </div>
                                   </div>
@@ -102,8 +109,9 @@ const MyListedNFT = ({account, nft, marketplace}) => {
                       <h2 className='px-10 2xl:text-[30px] xl:text-[30px] lg:text-[30px] text-[20px]'>Want to create your own NFT and list it to the store? <button onClick={()=>setShowCreate(true)} className='2xl:text-[25px] font-bold text-slate-900 hover:text-slate-600 duration-300'>Create here</button></h2>
                     </div>
                     {showCreate ? <CreateAndList nft={nft} marketplace={marketplace} setShowCreate={setShowCreate}/> : null}
+                    
                     <h1 className='2xl:text-[70px] xl:text-[70px] lg:text-[70px] text-[45px] pt-20 px-10 '>Sold Items</h1>
-                    {soldItems.length > 0 && loadSoldItems(soldItems)}
+                    {listedItems.length > 0 && loadSoldItems(soldItems)}
             </div>
         </div>
     </div>
