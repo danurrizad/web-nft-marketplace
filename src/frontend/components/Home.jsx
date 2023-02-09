6b4f6e02f6ec24371ee0479864b3c035729ded46
danurrizad <danurrizad242@gmail.com>
2023-02-09 20:42:22 +0700

    add Modal for buy and toastify

--- src/frontend/components/Home.jsx
+++ src/frontend/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 import { ethers } from 'ethers';
@@ -7,57 +7,135 @@ import LandingPage from './Landing.jsx'
 import Loading from './Loading';
 import Modal from './Modal';
 
+import MarketplaceAddress from '../contractsData/Marketplace-address.json';
+import MarketplaceABI from '../contractsData/Marketplace.json';
+import NFTAddress from '../contractsData/ERC721NFT-address.json';
+import NFTABI from '../contractsData/ERC721NFT.json';
 
-const Home = ({account, nft, marketplace}) => {
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const Home = ({account, nft, marketplace, setNFT, setMarketplace}) => {
     
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const idItemRef = useRef();
+    const totalPriceRef = useRef();
     const [showModal, setShowModal] = useState({
         title:"",
         img:"",
+        msg:"",
         isRunning:false
     });
 
-    const handleConfirm = (items) => {
-        setShowModal(true)
+    const handleModal = (title, img, msg, isRunning) => {
+        setShowModal({
+            title,
+            img,
+            msg,
+            isRunning,
+        })
+    }
+
+    const handleBuy = (items) => {
+        handleModal(items.name, items.image, "Are you sure want to buy this?", true);
+        idItemRef.current = items.itemId;
+        totalPriceRef.current = items.totalPrice
+    }
+
+    const confirmBuy = (yes) => {
+        if(yes){
+            try{
+                buyItems(idItemRef.current, totalPriceRef.current)
+                handleModal('',false)
+            }
+            catch(error){
+                handleModal('', false)
+            }
+        }
+        handleModal('', false)
     }
 
     const loadMarketplaceItems = async () => {
-        setIsLoading(true);
-        const itemCount = await marketplace.itemCount();                 // Load all unsold items
-        console.log(itemCount);
-        let items = []
-        for (let i = 1; i <= itemCount; i++) {
-            const item = await marketplace.items(i)
-            if (!item.sold) {                                       
-                const uri = await nft.tokenURI(item.tokenId)                // get uri url from nft contract
-                console.log(uri)
-                const response = await fetch(uri)                           // use uri to fetch the nft metadata stored on ipfs 
-                const metadata = await response.json()
-                const totalPrice = await marketplace.getTotalPrice(item.itemId)     // get total price of item (item price + fee)
-                items.push({                                // Add item to items array
-                    totalPrice,
-                    itemId: item.itemId,
-                    seller: item.seller,
-                    name: metadata.name,
-                    description: metadata.description,
-                    image: metadata.image
-                })
+        if(account){
+            setIsLoading(true);
+            const itemCount = await marketplace.itemCount();                 // Load all unsold items
+            console.log(itemCount);
+            let items = []
+            for (let i = 1; i <= itemCount; i++) {
+                const item = await marketplace.items(i)
+                if (!item.sold) {                                       
+                    const uri = await nft.tokenURI(item.tokenId)                // get uri url from nft contract
+                    console.log(uri)
+                    const response = await fetch(uri)                           // use uri to fetch the nft metadata stored on ipfs 
+                    const metadata = await response.json()
+                    const totalPrice = await marketplace.getTotalPrice(item.itemId)     // get total price of item (item price + fee)
+                    items.push({                                // Add item to items array
+                        totalPrice,
+                        itemId: item.itemId,
+                        seller: item.seller,
+                        name: metadata.name,
+                        description: metadata.description,
+                        image: metadata.image
+                    })
+                }
             }
+            setItems(items);
+            setIsLoading(false);
         }
-        setItems(items);
-        setIsLoading(false);
     }
 
-    const buyItems = async (item) => {
-        await (await marketplace.purchaseItem(item.itemId, { value: item.totalPrice })).wait()
+    const buyItems = async (itemId, totalPrice) => {
+        try{
+            await (await marketplace.purchaseItem(itemId, { value: totalPrice })).wait()
+            toast.success('Successed', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                });   
+        }
+        catch(error){
+            console.log('Ether not enough to send')
+            toast.error("You don't have enough ETH to buy this NFT", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                });
+        }
         loadMarketplaceItems()
       }
 
+    
+
     useEffect(() => {
-      loadMarketplaceItems()
+        
     }, [])
     
+
+    if(!account) return(
+        <>
+            <LandingPage/>
+            <div className='min-h-screen bg-slate-300 max-w-screen'>
+                <h1 className='text-center text-[72px] pb-12'>Store</h1>
+                    <div className='justify-center flex-1 text-center px-4 2xl:text-[30px] xl:text-[30px] lg:text-[30px] md:text-[30px] text-[15px]'>
+                        <h1 className='pt-1 pb-20'>Wallet address :</h1>
+                        <p>Please connect your wallet to access this store</p>
+                    </div>
+            </div>
+            <Footer/>
+        </>
+    )
 
     return (
     <div>
@@ -71,7 +149,6 @@ const Home = ({account, nft, marketplace}) => {
                     <h1 className='text-center text-[72px] pb-12'>Store</h1>
                     <div className='justify-center flex-1 text-center px-4 2xl:text-[30px] xl:text-[30px] lg:text-[30px] md:text-[30px] text-[15px]'>
                         <h1 className='pt-1'>Wallet address :</h1>
-                        {isLoading ? <Loading textLoading='Loading items...'/> : null}
                         {account ? (<div className='flex justify-center'><h1 className='bg-slate-100 px-4 py-1 rounded-full shadow-lg shadow-black'>{account}</h1></div>) : null}
                         
                         <div className='px-4 2xl:text-[28px] xl:text-[28px] lg:text-[28px] md:text-[28px] text-[15px]'>
@@ -92,8 +169,8 @@ const Home = ({account, nft, marketplace}) => {
                                         <div className='text-center 2xl:text-[20px] text-[10px]'>
                                             <p className='text-left px-4'>{item.description}</p>
                                             <div className='bg-slate-700 border-y-4 border-black p-4'>
-                                                <button className='bg-green-300 2xl:py-1 xl:py-1 lg:py-1 md:py-1 sm:py-1 px-4 rounded-3xl hover:bg-green-500 duration-200'
-                                                    onClick={()=>buyItems(item)}
+                                                <button className='transition ease-in-out duration-200 bg-emerald-300 hover:bg-emerald-500 2xl:py-1 xl:py-1 lg:py-1 md:py-1 sm:py-1 px-4 rounded-3xl  '
+                                                    onClick={()=>handleBuy(item)}
                                                     >Buy ({ethers.utils.formatEther(item.totalPrice)} ETH)
                                                 </button>
                                             </div>
@@ -101,13 +178,15 @@ const Home = ({account, nft, marketplace}) => {
                                     </div>
                                 ))}
 
+                                {showModal.isRunning && <Modal onDialog={confirmBuy} msg={showModal.msg} itemName={showModal.title} itemImage={showModal.img}/>}
                                 {/*{showModal ? <Modal onConfirm={confirmBuy}/> : null}*/}
+                                <ToastContainer />
                             </div>
                         </div>
                     ):(
                         <main className='py-8 text-center px-4'>
                             <h1 className='2xl:text-[40px] xl:text-[40px] lg:text-[40px] text-[20px]'>Unfortunately, there are no listed NFTs here for a moment</h1>
-                            <h1 className='2xl:text-[35px] xl:text-[35px] lg:text-[35px] text-[18px]'>Please comeback later...</h1>
+                            <button className='2xl:text-[35px] xl:text-[35px] lg:text-[35px] text-[18px] bg-white px-4 rounded-xl shadow-xl hover:bg-slate-100' onClick={loadMarketplaceItems}>Refresh item here</button>
                         </main>
                     )}
                 </div>
