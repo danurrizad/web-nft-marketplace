5b54ee9e0f952754c86323f7f45c44b588be1ad4
danurrizad <danurrizad242@gmail.com>
2023-02-09 20:41:54 +0700

    update footer

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Footer = () => {
   return (
     <div id='contact' className='bg-slate-800 p-8'>
-        <div>
-            <ul className='text-white text text-center text-[24px] py-10'>
-                <li><a href='#home'>Home</a></li>
-                <li><a href='#store'>Store</a></li>
-                <li><a href='#contact'>Contact</a></li>
-            </ul>
-        </div>
+      <div>
+        
+      </div>
+      <div>
+          <ul className='text-white items-center justify-center text-[13px] 2xl:text-[22px] xl:text-[22px] lg:text-[22px] md:text-[18px] sm:text-[15px] py-10 flex gap-4 border-t-2  border-slate-700'>
+              <Link to="/" className='hover:text-slate-300 '>HOME</Link>
+              <Link to="/" className='hover:text-slate-300'>STORE</Link>
+              <Link to="/manage" className='hover:text-slate-300'>MANAGE</Link>
+              <Link to="/my-purchases" className='hover:text-slate-300'>MY PURCHASES</Link>
+          </ul>
+      </div>
+      <div className='flex justify-center border-t-2  border-slate-700 text-white pt-8'>
+        <p>Copyright © 2023 </p>
+      </div>
     </div>
   )
 }
